d22158c2a3a569bf8b5a6b895d875ccf89cc3a03
Arnur Nigmetov <nigmetov@tugraz.at>
2018-10-19 18:27:11 +0200

    Parallelize experiments in python

--- python_version/greedy_spanner.py
+++ python_version/greedy_spanner.py
@@ -7,11 +7,22 @@ import sys
 
 inf_dist = 10000000000
 
-dim = 2
 
+class QuasiSortedDistances:
+    def __init__(self, qsorted_distances, n_buckets):
+        self.qsorted_distances = qsorted_distances
+        self.n_buckets = n_buckets
 
-def get_points(n_points):
+def get_points(n_points, dim):
     return np.random.randn(n_points, dim)
+
+
+def get_exponential_points(n_points, dim):
+    max_power = max(n_points / 2.0, 10.0)
+    print(f"max_power = {max_power}")
+    shape = (n_points, dim)
+    res = np.random.uniform(1.0, max_power, shape)
+    return np.power(2.0, res)
 
 
 def distance_matrix(points):
@@ -46,23 +57,10 @@ def get_distance(a, b):
     return np.linalg.norm(a - b)
 
 
-# def is_distance_larger(a, b, t):
-#     """Return true, if d(a, b) > t;
-#        return false, if d(a,b) < t/2; if d(a,b) is between t/2 and t,
-#        return either true or false"""
-#     d = get_distance(a, b)
-#     if d > t:
-#         return True
-#     elif d < t / 2:
-#         return False
-#     else:
-#         return np.random.uniform() < 0.5
-
-
-def get_buckets(max_dist, n_buckets):
-    scaling_factor = max_dist / (2 ** (n_buckets))
-    result = [(0.0, scaling_factor * 2.0)]
-    result.extend([(scaling_factor * (2 ** k), 4.0 * scaling_factor * (2 ** k)) for k in range(n_buckets - 1)])
+def get_buckets(min_dist, max_dist):
+    n_buckets = int(np.ceil(np.log2(max_dist/min_dist)))
+    scaling_factor = min_dist
+    result = [(scaling_factor * (2 ** k), 4.0 * scaling_factor * (2 ** k)) for k in range(n_buckets)]
     return result
 
 
@@ -85,18 +83,20 @@ def get_bucket_index(buckets, d):
         return cand_list[idx]
 
 
-def quasi_sorted_distances(points, n_buckets, dist_list=None):
+def quasi_sorted_distances(points, dist_list=None):
     if dist_list is None:
         n_points = len(points)
         dm = distance_matrix(points)
         dist_list = [(dm[i][j], i, j) for i in range(n_points) for j in range(i + 1, n_points)]
     max_dist = max(dist_list)[0]
-    buckets = get_buckets(max_dist, n_buckets)
+    min_dist = min(dist_list)[0]
+    buckets = get_buckets(min_dist, max_dist)
+    n_buckets = len(buckets)
     result = [[] for i in range(n_buckets)]
     for d in dist_list:
         result[get_bucket_index(buckets, d)].append(d)
     map(random.shuffle, result)
-    return [x for y in result for x in y]
+    return QuasiSortedDistances([x for y in result for x in y], n_buckets)
 
 
 def greedy_spanner(points, eps, sd=None):
@@ -115,29 +115,43 @@ def greedy_spanner(points, eps, sd=None):
 
 if __name__ == "__main__":
 
-    # We like goblinses, batses and fishes. But we hasn't tried Hobbitses before.
-    n_pointses = [50, 100, 200, 500, 1000, 2000]
-    n_bucketses = [10, 20, 30]
-    epsilons = [0.01, 0.1, 0.2, 0.5]
+    # min_dist = 0.01
+    # max_dist = 3.0
+    # print(get_buckets(min_dist, max_dist))
 
-    for n_points in n_pointses:
-        points = get_points(n_points)
-        dist_list = distances_list(points)
-        for eps in epsilons:
+    n_pointses = [50, 100]
+    epsilons = [0.01, 0.1, 0.5]
+    dims = [3, 5, 7]
 
-            gs = greedy_spanner(points, eps, sorted(dist_list))
-            total_n_pairs = n_points * (n_points - 1) / 2.0
-            n_gs_edges = gs.number_of_edges()
-            sparseness = n_gs_edges / total_n_pairs
-            print(
-                f"eps = {eps}, #points = {n_points}, greedy spanner sparseness = {n_gs_edges} / {total_n_pairs}  = {sparseness}")
+    for dim in dims:
+        for n_points in n_pointses:
+            # points = get_points(n_points, dim)
+            points = get_exponential_points(n_points, dim)
+            dist_list = distances_list(points)
+            max_dist = max(dist_list)[0]
+            for eps in epsilons:
 
-            for n_buckets in n_bucketses:
-                sd = quasi_sorted_distances(points, n_buckets, dist_list)
-                gs = greedy_spanner(points, eps, sd)
+                gs = greedy_spanner(points, eps, sorted(dist_list))
+                total_n_pairs = n_points * (n_points - 1) / 2.0
+                n_gs_edges = gs.number_of_edges()
+                sparseness = n_gs_edges / total_n_pairs
+                print(
+                    f"dim = {dim}, eps = {eps}, #points = {n_points}, greedy spanner sparseness = {n_gs_edges} / {total_n_pairs}  = {sparseness}")
+
+                random_dist_list = dist_list[:]
+                random.shuffle(random_dist_list)
+                gs_random = greedy_spanner(points, eps, random_dist_list)
+
+                n_gs_random_edges = gs_random.number_of_edges()
+                sparseness_random = n_gs_random_edges / total_n_pairs
+                print(
+                    f"dim = {dim}, eps = {eps}, #points = {n_points}, random order spanner sparseness = {n_gs_random_edges} / {total_n_pairs}  = {sparseness_random}")
+
+                sd = quasi_sorted_distances(points, dist_list)
+                gs = greedy_spanner(points, eps, sd.qsorted_distances)
                 n_gs_quasi_edges = gs.number_of_edges()
                 sparseness = n_gs_quasi_edges / total_n_pairs
                 print(
-                    f"eps = {eps}, #points = {n_points}, #buckets =  {n_buckets} with quasi-sorted distances sparseness = {n_gs_quasi_edges} / {total_n_pairs}  = {sparseness}")
+                    f"dim = {dim}, eps = {eps}, #points = {n_points}, #buckets =  {sd.n_buckets} with quasi-sorted distances sparseness = {n_gs_quasi_edges} / {total_n_pairs}  = {sparseness}")
 
     sys.exit(0)
