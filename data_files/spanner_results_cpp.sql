1077b6c40802469bfa29ebb6e0c2341fadc26cea
Arnur Nigmetov <nigmetov@tugraz.at>
2018-11-24 21:22:50 +0100

    Interim

---
+++ data_files/spanner_results_cpp.sql
@@ -0,0 +1,34 @@
+﻿SELECT spanner_method, dim, n_points, epsilon, points_generation_method, 
+       input_file, n_edges, sparseness, n_edges_to_n_points_ratio
+  FROM public.spanner_results_cpp;
+
+
+select distinct spanner_method
+from public.spanner_results_cpp;
+
+select distinct points_generation_method
+from public.spanner_results_cpp;
+
+"exp"
+"clustered"
+"unif"
+"normal"
+"unif_100"
+
+
+SELECT spanner_method, dim, n_points, epsilon, points_generation_method, 
+       n_edges, sparseness, n_edges_to_n_points_ratio
+FROM public.spanner_results_cpp t
+where spanner_method = 'greedy-non-blind'
+and abs(epsilon - 0.1) < 0.001
+and dim = 2
+and t.points_generation_method = 'unif'
+
+
+SELECT spanner_method, dim, epsilon, points_generation_method, 
+       corr(n_edges, n_points),
+       regr_slope(n_edges, n_points)
+FROM public.spanner_results_cpp t
+where spanner_
+group by spanner_method, dim, epsilon, points_generation_method
+order by epsilon, points_generation_method, spanner_method
--- python_version/spanner_experiment_data/plots.py
+++ python_version/spanner_experiment_data/plots.py
@@ -1,29 +1,90 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import sys
 
-ds = pd.read_csv("all_data.txt", header=0, sep=';')
+# ds = pd.read_csv("all_data.txt", header=0, sep=';')
+#
+# index_cols = ['dim', 'n_points', 'point_method', 'epsilon']
+# ds_greedy = ds.loc[(ds['spanner_method'] == 'greedy')].set_index(index_cols, drop=False)
+# ds_random = ds.loc[(ds['spanner_method'] == 'random')].set_index(index_cols, drop=False)
+# ds_quasi = ds.loc[(ds['spanner_method'] == 'quasi')].set_index(index_cols, drop=False)
+# ds_blind_greedy = ds.loc[(ds['spanner_method'] == 'blind_greedy')].set_index(index_cols, drop=False)
+#
+# ds_greedy.drop(columns=['spanner_method'], inplace=True)
+# ds_random.drop(columns=['spanner_method'], inplace=True)
+# ds_quasi.drop(columns=['spanner_method'], inplace=True)
+# ds_blind_greedy.drop(columns=['spanner_method'], inplace=True)
+#
+# ds_all_dims = ds_greedy.join(ds_random, lsuffix='_greedy', rsuffix='_random').join(ds_quasi, rsuffix='_quasi').join(
+#     ds_blind_greedy, rsuffix='_blind_greedy')
+#
+# print(ds_all_dims.head())
+# print(ds_all_dims.describe(include='all').to_string())
+#
+# ycols = ["sparseness_random", "sparseness_greedy", "sparseness", "sparseness_blind_greedy"]
+# # ycols = ["sparseness_random", "sparseness_greedy", "sparseness_blind_greedy"]
+# mask_1 = (ds_all_dims['dim'] == 2) & (ds_all_dims['point_method'] == 'get_exponential_points') & (ds_all_dims['epsilon'] == 0.01)
+# ds_all_dims.loc[mask_1].plot(x="n_points", y=ycols)
+# # mask_1 = (ds_all_dims['n_points'] == 200) & (ds_all_dims['point_method'] == 'get_exponential_points') & (ds_all_dims['epsilon'] == 0.01)
+# # ds_all_dims.loc[mask_1].plot(x="dim", y=ycols)
+# plt.show()
 
-index_cols = ['dim', 'n_points', 'point_method', 'epsilon']
-ds_greedy = ds.loc[(ds['spanner_method'] == 'greedy')].set_index(index_cols, drop=False)
-ds_random = ds.loc[(ds['spanner_method'] == 'random')].set_index(index_cols, drop=False)
-ds_quasi = ds.loc[(ds['spanner_method'] == 'quasi')].set_index(index_cols, drop=False)
-ds_blind_greedy = ds.loc[(ds['spanner_method'] == 'blind_greedy')].set_index(index_cols, drop=False)
 
-ds_greedy.drop(columns=['spanner_method'], inplace=True)
-ds_random.drop(columns=['spanner_method'], inplace=True)
-ds_quasi.drop(columns=['spanner_method'], inplace=True)
-ds_blind_greedy.drop(columns=['spanner_method'], inplace=True)
+if __name__ == "__main__":
+    ds = pd.read_csv("spanner_cpp_logs.txt", header=0, sep=';')
+    # print(ds.point_gen_method.unique())
+    # print(ds.spanner_method.unique())
+    # index_cols = ['dim', 'point_gen_method', 'input_file', 'spanner_method', 'epsilon', 'n_points', 'n_edges', 'sparseness', 'edges_to_points_ratio']
+    index_cols = ['dim', 'point_gen_method', 'input_file', 'spanner_method', 'epsilon', 'n_points']
+    # ds.drop(columns=['sparseness', 'edges_to_points_ratio'], inplace=True)
+    # index_cols = ['dim', 'point_gen_method', 'input_file', 'spanner_method', 'epsilon', 'n_points']
 
-ds_all_dims = ds_greedy.join(ds_random, lsuffix='_greedy', rsuffix='_random').join(ds_quasi, rsuffix='_quasi').join(
-    ds_blind_greedy, rsuffix='_blind_greedy')
+    ds_non_blind_greedy = ds.loc[(ds['spanner_method'] == 'greedy-non-blind')].set_index(index_cols, drop=False)
+    ds_blind_greedy = ds.loc[(ds['spanner_method'] == 'blind-greedy')].set_index(index_cols, drop=False)
+    ds_blind_random = ds.loc[(ds['spanner_method'] == 'blind-random')].set_index(index_cols, drop=False)
+    ds_quasi_greedy = ds.loc[(ds['spanner_method'] == 'blind-quasi-sorted-greedy')].set_index(index_cols, drop=False)
+    ds_quasi_shaker = ds.loc[(ds['spanner_method'] == 'blind-quasi-sorted-shaker')].set_index(index_cols, drop=False)
+    ds_blind_random_bad_ratio = ds.loc[(ds['spanner_method'] == 'blind-random-bad-ratio')].set_index(index_cols,
+                                                                                                     drop=False)
+    ds_blind_random_bad_ratio_lbf = ds.loc[
+        (ds['spanner_method'] == 'blind-random-bad-ratio-lower-bound-first')].set_index(index_cols, drop=False)
+    ds_blind_random_bad_ratio_cf = ds.loc[(ds['spanner_method'] == 'blind-random-bad-ratio-connect-first')].set_index(
+        index_cols, drop=False)
+    ds_blind_random_bad_ratio_cf_lbf = ds.loc[
+        (ds['spanner_method'] == 'blind-random-bad-ratio-connect-first-lower-bound-first')].set_index(index_cols,
+                                                                                                      drop=False)
 
-print(ds_all_dims.head())
-print(ds_all_dims.describe(include='all').to_string())
+    # print(ds_non_blind_greedy.head(20))
+    print(ds_quasi_shaker.head(20))
+    print("********************************************************************************")
+    sys.exit(0)
 
-ycols = ["sparseness_random", "sparseness_greedy", "sparseness", "sparseness_blind_greedy"]
-# ycols = ["sparseness_random", "sparseness_greedy", "sparseness_blind_greedy"]
-mask_1 = (ds_all_dims['dim'] == 2) & (ds_all_dims['point_method'] == 'get_exponential_points') & (ds_all_dims['epsilon'] == 0.01)
-ds_all_dims.loc[mask_1].plot(x="n_points", y=ycols)
-# mask_1 = (ds_all_dims['n_points'] == 200) & (ds_all_dims['point_method'] == 'get_exponential_points') & (ds_all_dims['epsilon'] == 0.01)
-# ds_all_dims.loc[mask_1].plot(x="dim", y=ycols)
-plt.show()
+    ds_non_blind_greedy.drop(columns=['spanner_method'], inplace=True)
+    # ds_blind_greedy.drop(columns=['spanner_method'], inplace=True)
+    # ds_blind_random.drop(columns=['spanner_method'], inplace=True)
+    # ds_quasi_greedy.drop(columns=['spanner_method'], inplace=True)
+    # ds_quasi_shaker.drop(columns=['spanner_method'], inplace=True)
+    # ds_blind_greedy.drop(columns=['spanner_method'], inplace=True)
+    ds_blind_random_bad_ratio.drop(columns=['spanner_method'], inplace=True)
+    ds_blind_random_bad_ratio_lbf.drop(columns=['spanner_method'], inplace=True)
+    ds_blind_random_bad_ratio_cf.drop(columns=['spanner_method'], inplace=True)
+    ds_blind_random_bad_ratio_cf_lbf.drop(columns=['spanner_method'], inplace=True)
+
+    ds_all_dims = ds_non_blind_greedy.join(ds_blind_greedy, lsuffix='_nbg', rsuffix='_bg')
+    ds_all_dims = ds_all_dims.join(ds_blind_random, rsuffix='_br')
+    ds_all_dims = ds_all_dims.join(ds_quasi_greedy, rsuffix='_qg').join(ds_quasi_shaker, rsuffix='_qs')
+    ds_all_dims = ds_all_dims.join(ds_blind_random_bad_ratio, rsuffix='_rbr')
+    ds_all_dims = ds_all_dims.join(ds_blind_random_bad_ratio_lbf, rsuffix='_rbr_lbf')
+    ds_all_dims = ds_all_dims.join(ds_blind_random_bad_ratio_cf, rsuffix='_rbr_cf')
+    ds_all_dims = ds_all_dims.join(ds_blind_random_bad_ratio_cf_lbf, rsuffix='_rbr_cf_lbf')
+
+    # print(ds_all_dims.head())
+    # print(ds_all_dims.describe(include='all').to_string())
+
+    ycols = ["sparseness_nbg", "sparseness_bg"]
+    # # ycols = ["sparseness_random", "sparseness_greedy", "sparseness_blind_greedy"]
+    mask_1 = (ds_all_dims['dim'] == 2) & (ds_all_dims['point_gen_method'] == 'unif') & (ds_all_dims['epsilon'] == 0.1)
+    aaa = ds_all_dims.loc[mask_1]
+    ds_all_dims.loc[mask_1].plot(x="n_points_nbg", y=ycols)
+    # # mask_1 = (ds_all_dims['n_points'] == 200) & (ds_all_dims['point_method'] == 'get_exponential_points') & (ds_all_dims['epsilon'] == 0.01)
+    # # ds_all_dims.loc[mask_1].plot(x="dim", y=ycols)
+    plt.show()
