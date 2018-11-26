2e17937afc97ad799824c40f4ed16628a6a58cec
Arnur Nigmetov <nigmetov@tugraz.at>
2018-11-27 00:26:54 +0100

    interim

---
+++ python_version/ann_experiment_data/ann_results_euclidean.sql
@@ -0,0 +1,30 @@
+﻿SELECT dim, epsilon, points_generation_method, query_generation_method, 
+	regr_slope(min_distances_number, log(n_points)) as min_dist_regr_degree,
+	regr_slope(max_distances_number, log(n_points)) as max_dist_regr_degree,
+	regr_slope(average_distances_number, log(n_points)) as avg_dist_regr_degree,
+	count(*)
+FROM public.ann_results_euclidean
+where points_generation_method = 'unif' and query_generation_method = 'query_1'
+group by dim, epsilon, points_generation_method, query_generation_method
+order by dim, epsilon, points_generation_method, query_generation_method
+
+
+SELECT dim, epsilon, points_generation_method, query_generation_method, 
+	regr_slope(min_distances_number, log(n_points)) as min_dist_regr_degree,
+	regr_slope(max_distances_number, log(n_points)) as max_dist_regr_degree,
+	regr_slope(average_distances_number, log(n_points)) as avg_dist_regr_degree,
+	count(*)
+FROM public.ann_results_euclidean
+where points_generation_method = 'unif' and query_generation_method = 'query_1'
+group by dim, epsilon, points_generation_method, query_generation_method
+order by dim, epsilon, points_generation_method, query_generation_method
+
+
+
+SELECT *
+FROM public.ann_results_euclidean
+where dim = 20 
+      and epsilon < 0.49 
+      and points_generation_method = 'unif'
+      
+
---
+++ python_version/import_ann_to_db.sql
@@ -0,0 +1,5 @@
+﻿delete from ann_results_euclidean
+where n_points >= 20000;
+
+copy ann_results_euclidean(dim, epsilon, n_points, points_generation_method, query_generation_method, min_fraction, max_fraction, avg_fraction, min_distances_number, max_distances_number, average_distances_number) 
+from '/home/narn/hdd_data/code/dynamic_spanner/python_version/ann_results.txt' delimiter ';' csv header 1;
