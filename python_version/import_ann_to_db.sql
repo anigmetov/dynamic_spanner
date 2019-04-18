b63fb11e3cc794b00241427559e0d4afcad7de38
Arnur Nigmetov <nigmetov@tugraz.at>
2019-04-19 00:34:39 +0200

    interim

--- python_version/import_ann_to_db.sql
+++ python_version/import_ann_to_db.sql
@@ -13,7 +13,29 @@ from ann_results_euclidean
 --where n_points >= 20000;
 
 copy ann_results_euclidean_density(dim, epsilon, n_points, points_generation_method, query_generation_method, min_fraction, max_fraction, avg_fraction, min_distances_number, max_distances_number, average_distances_number) 
-from '/home/narn/hdd_data/code/dynamic_spanner/python_version/ann_experiment_data/ann_density_dependency.txt' delimiter ';' csv 
+from '/home/narn/hdd_data/code/dynamic_spanner/python_version/ann_experiment_data/nn_mcgill_original_results.csv' delimiter ';' csv 
+
+
+copy ann_results_mcgill(fname, epsilon, n_points, min_fraction, max_fraction, avg_fraction, min_distances_number, max_distances_number, average_distances_number) 
+from '/home/narn/hdd_data/code/dynamic_spanner/python_version/ann_experiment_data/nn_mcgill_original_results.csv' delimiter ';' csv 
+
+
+update ann_results_mcgill t
+set wasserstein_degree = case 
+                           when fname like '%q_1%' then 1 
+                           when fname like '%q_2%' then 2 
+                           when fname like '%q_3%' then 3 
+                         end,
+   dim = case                when fname like '%dim_0%' then 0
+                           when fname like '%dim_1%' then 1 
+                           when fname like '%dim_2%' then 2 
+                         end
+where t.fname like '%/%'
+
+select *
+from ann_results_mcgill t
+where t.fname like '%/%'
+set 
 
 
 select count(*)
